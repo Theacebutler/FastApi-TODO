15303ae7546eaf626bff766151fa1328eb7543ef
theacebutler <theacebutler@gmail.com>
2025-11-05 23:19:55 -0500

    added formating to task list

--- bash/get.py
+++ bash/get.py
@@ -1,8 +1,9 @@
 import requests
-
+from datetime import datetime
 base_url = "http://127.0.0.1:8000"
 
 tasks = requests.get(base_url + "/tasks/all").json()
-
-
-print(tasks)
+x = str(tasks[0]['created_at'][:10])+ ' ' + str(tasks[0]['created_at'][11:-7])
+for task in tasks:
+    date = datetime.strptime(x, "%Y-%m-%d %H:%M:%S")
+    print(f"{date} | task id: {task['id']} | Title: {task['title']} | Description: {task['description']}\n")
