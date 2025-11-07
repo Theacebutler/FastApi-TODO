405ce276f1b178f08caa5fdcaf235b5f681e96a0
theacebutler <theacebutler@gmail.com>
2025-11-06 20:18:48 -0500

    added -o flag for open todos

--- bash/get.py
+++ bash/get.py
@@ -1,8 +1,11 @@
 import requests
+from sys import argv
 from datetime import datetime
 base_url = "http://127.0.0.1:8000"
-
-tasks = requests.get(base_url + "/tasks/all").json()
+completed = ""
+if "-o" in argv:
+    completed = "open"
+tasks = requests.get(base_url + f"/tasks/all/{completed}").json()
 x = str(tasks[0]['created_at'][:10])+ ' ' + str(tasks[0]['created_at'][11:-7])
 for task in tasks:
     date = datetime.strptime(x, "%Y-%m-%d %H:%M:%S")
