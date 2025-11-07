024632dabd56ea852a781eb206cc74f4a91ef2a0
theacebutler <theacebutler@gmail.com>
2025-11-06 20:20:00 -0500

    minor changs

--- app/schemas.py
+++ app/schemas.py
@@ -1,15 +1,18 @@
-from typing import Any
-from pydantic import BaseModel, Field
 from datetime import datetime
+
+from pydantic import BaseModel
 
 
 class Task(BaseModel):
-    ''' an object to creaet at the post route'''
+    """an object to creaet at the post route"""
+
     title: str
     description: str | None
 
+
 class ResponseTask(BaseModel):
-    '''a response object, adding the id and date created'''
+    """a response object, adding the id and date created"""
+
     id: int
     title: str
     description: str | None
